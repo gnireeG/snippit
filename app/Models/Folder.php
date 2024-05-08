288bad59db7193bc9bc9660915b554addd217e3d
gnireeG <48487313+gnireeG@users.noreply.github.com>
2024-05-08 12:08:17 +0200

    add Folder Model

---
+++ app/Models/Folder.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\Rule;
+
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\belongsTo;
+
+class Folder extends Model
+{
+    protected $fillable = ['name', 'parent_id', 'team_id', 'slug'];
+
+    public function getPath()
+    {
+        $path = [];
+
+        $currentFolder = $this;
+
+        while ($currentFolder) {
+            $path[] = [
+                'name' => $currentFolder->name,
+                'slug' => $currentFolder->slug, // Assuming you have a 'slug' attribute in your Folder model
+            ];
+
+            $currentFolder = $currentFolder->parentFolder;
+        }
+
+        return array_reverse($path);
+    }
+
+    // Define the one-to-many relationship with Snippit
+    public function snippits(): hasMany
+    {
+        return $this->hasMany(Snippit::class);
+    }
+
+    // Define the self-referential relationship for subfolders
+    public function subfolders(): hasMany
+    {
+        return $this->hasMany(Folder::class, 'parent_id');
+    }
+
+    // Define the inverse of the self-referential relationship
+    public function parentFolder(): belongsTo
+    {
+        return $this->belongsTo(Folder::class, 'parent_id');
+    }
+
+    // Define the team relationship
+    public function team(): belongsTo
+    {
+        return $this->belongsTo(Team::class);    
+    }
+
+    // Define the validation rules
+    public static function validationRules($parentId = null)
+    {
+        $uniqueNameRule = Rule::unique('folders', 'name')->where(function ($query) use ($parentId) {
+            $query->where('parent_id', $parentId);
+        });
+
+        return [
+            'name' => ['required', 'string', 'max:255', $uniqueNameRule],
+        ];
+    }
+}
